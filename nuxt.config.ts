3794498f8dcf035bc5909b380636a116c7fc9779
theo-ooooo <kkw.theo@gmail.com>
2025-09-17 15:36:31 +0900

    chore: nuxt config api base add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    public: {
+      apiBase: "http://localhost:8000",
+    },
+  },
 });
